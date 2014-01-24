4e19e475f062d8c42a89bf8238da394f2887c8d1
Stefan Foulis <stefan@foulis.ch>
2014-01-24 15:23:54 +0100

    fixes style choices addon setting

--- cmscloud_config.py
+++ cmscloud_config.py
@@ -22,5 +22,5 @@ class Form(forms.BaseForm):
     class_names = ClassNamesField('Class names', max_length=255)
 
     def to_settings(self, data, settings):
-        settings['ALDRYN_STYLE_CLASS_NAMES'] = list(filter(bool, map(lambda x: x.strip(), data['class_names'].split(','))))
+        settings['ALDRYN_STYLE_CLASS_NAMES'] = [(class_name, class_name) for class_name in set(filter(bool, map(lambda x: x.strip(), data['class_names'].split(','))))]
         return settings
