106ffcc32afaec5ce3867ab7542f2e2ce128de0e
Stefan Foulis <stefan@foulis.ch>
2014-01-24 15:03:51 +0100

    fixes class_name validation in controlpanel

--- cmscloud_config.py
+++ cmscloud_config.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from cmscloud_client import forms
 import re
+from cmscloud_client import forms
+
 
 CLASS_NAME_FORMAT = re.compile(r'^\w[\w_-]*$')
 
@@ -9,16 +10,16 @@ class ClassNamesField(forms.CharField):
 
     def clean(self, value):
         value = super(ClassNamesField, self).clean(value)
-        value = filter(bool, map(lambda x: x.strip(), value.split(',')))
-        for class_name in value:
+        class_names = filter(bool, map(lambda x: x.strip(), value.split(',')))
+        for class_name in class_names:
             if not CLASS_NAME_FORMAT.match(class_name):
                 raise forms.ValidationError(u'%s is not a proper class name.' % (class_name, ))
-        return value
+        return u", ".join(class_names)
 
 
 class Form(forms.BaseForm):
 
-    class_names = ClassNamesField('Class names', max_length=100)
+    class_names = ClassNamesField('Class names', max_length=255)
 
     def to_settings(self, data, settings):
         settings['ALDRYN_STYLE_CLASS_NAMES'] = data['class_names']
